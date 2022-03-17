77d5dff26a7d11e5dc68edca3ac4d1b15e33eb28
Ignacio Sánchez <nachosanchezh@hotmail.com>
2022-03-17 12:46:14 +0100

    Ejercicio 1.2 hecho

--- Práctica 0/Práctica 0.py
+++ Práctica 0/Práctica 0.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
+# =============================================================================
+# Ignacio Sánchez Herrera
+# Práctica 0 - Aprendizaje Automático
+# =============================================================================
 
 from sklearn.datasets import load_iris
 import numpy as np
@@ -41,7 +40,8 @@ c_virginica = data[data[:, 0] == 2., : ]
 
 
 
-# Pintamos las caracterísiticas en un Scatter Plot.
+# Pintamos las caracterísiticas en un Scatter Plot con el color y la etiqueta 
+# correspondiente
 plt.scatter(c_setosa[:, 1], c_setosa[:, 2], c='red', label=target_names[0])
 plt.scatter(c_versicolor[:, 1], c_versicolor[:, 2], c='green', label=target_names[1])
 plt.scatter(c_virginica[:, 1], c_virginica[:, 2], c='blue', label=target_names[2])
@@ -79,12 +79,12 @@ virginica_test = virginica_data[training_split : ]
 
 # Unimos los arrays de training en uno solo
 training_data = np.concatenate((setosa_training, versicolor_training, virginica_training))
-# Ordenamos los índices
+# Ordenamos los índices para que NO nos queden agrupados por clases
 training_data = np.sort(training_data)
 
 # Unimos los arrays de test en uno solo
 test_data = np.concatenate((setosa_test, versicolor_test, virginica_test))
-# Ordenamos los índices
+# Ordenamos los índices para que NO nos queden agrupados por clases
 test_data = np.sort(test_data)
 
 
@@ -106,43 +106,52 @@ print(data[test_data, 0])
 # =============================================================================
 # Ejercicio 3
 # =============================================================================
+# Obtenemos 100 valores equiespaciados entre 0 y 4pi mediante la función
+# np.linspace()
 vals = np.linspace(0, 4*np.pi, num=100)
+
+# Declaramos las funciones lambda
 f1 = lambda x : 10**(-5) * np.sinh(x)
 f2 = lambda x : np.cos(x)
 f3 = lambda x : np.tanh(2 * np.sin(x) - 4 * np.cos(x))
 
+# Evaluamos los valores en cada una de las funciones
 f1_vals = f1(vals)
 f2_vals = f2(vals)
 f3_vals = f3(vals)
 
+# Dibujamos las gráficas con los colores y etiquetas correspondientes
 plt.plot(vals, f1_vals, 'g--', label='y = 1e-5*sinh(x)')
 plt.plot(vals, f2_vals, 'k--', label='y = cos(x)')
 plt.plot(vals, f3_vals, 'r--', label='y = tanh(2*sin(x)-4*cos(x))')
-
+# Dibujamos la leyenda en la escquina superior izquierda
 plt.legend(loc='upper left')
+# Mostramos la figura
 plt.show()
 
 # =============================================================================
 # Ejercicio 4
 # =============================================================================
-# Definimos las dos funciones
+# Definimos las dos funciones lambda
 f1 = lambda x, y: 1 - np.abs(x + y) - np.abs(y - x)
 f2 = lambda x, y: x * y * np.exp(-(x**2)-(y**2))
 
-# set up a figure twice as wide as it is tall
+# Fijamos el tamaño de la figura para que se vea de manera adecuada
 fig = plt.figure(figsize=(20, 20))
 
 # =============
 # Primer subplot
 # =============
-# set up the axes for the first plot
+# Añadimos un subplot con proyección 3D, de 2 columnas y 1 fila
+# en la primera posición
 ax = fig.add_subplot(1, 2, 1, projection='3d')
 # Le ponemos título a la figura con un tamaño de fuente 20
 ax.set_title('Pirámide', fontsize=20)
 
 # Generamos los valores de x e y
-X = np.arange(-6, 6.4, 0.4)
-Y = np.arange(-6, 6.4, 0.4)
+# 30 valores entre -6 y 6 equiespaciados
+X = np.linspace(-6, 6, num=30)
+Y = np.linspace(-6, 6, num=30)
 # Obtenemos las matrices de coordenadas a partir de X e Y
 X, Y = np.meshgrid(X, Y)
 # Evaluamos los valores
@@ -158,14 +167,16 @@ ax.yaxis.set_ticks(np.arange(-6, 7, 2))
 # ==============
 # Segundo subplot
 # ==============
-# set up the axes for the first plot
+# Añadimos un subplot con proyección 3D, de 2 columnas y 1 fila
+# en la segunda posición
 ax = fig.add_subplot(1, 2, 2, projection='3d')
 # Le ponemos título a la figura
 ax.set_title('$x \cdot y \cdot e^{(-x^2 -y^2)}$', fontsize=20)
 # Generamos los valores de x e y
-X = np.arange(-2, 2, 0.05)
-Y = np.arange(-2, 2, 0.05)
-# Obtenemos las matrices de coordenadas a partir de X e Y
+# 80 valores equiespaciados entre -2 y 2
+X = np.linspace(-2, 2, num=80)
+Y = np.linspace(-2, 2, num=80)
+# Obtenemos la malla de coordenadas a partir de X e Y
 X, Y = np.meshgrid(X, Y)
 # Evaluamos los valores
 Z = f2(X, Y)
@@ -177,4 +188,5 @@ ax.set_zlim(-0.18, 0.18)
 ax.xaxis.set_ticks(np.arange(-2, 2.5, 0.5))
 ax.yaxis.set_ticks(np.arange(-2, 2.5, 0.5))
 
+# Mostramos la figura
 plt.show()
